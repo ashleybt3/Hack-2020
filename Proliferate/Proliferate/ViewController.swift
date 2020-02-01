cdc589bcfffa70fc84882ee385b1c147f46f8d46
Kastur Koul <kasturkoul@gmail.com>
2020-02-01 12:47:23 -0800

    Opening page and app photo

---
+++ Proliferate/Proliferate/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Proliferate
+//
+//  Created by Kastur Koul on 2/1/20.
+//  Copyright © 2020 Kastur Koul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
+
